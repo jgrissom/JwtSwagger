f6f91cd28eb16286953ffb06823ace5bc6a9846c
jgrissom <jgrissom91@gmail.com>
2021-09-26 22:53:03 -0500

    [2.06] Return specific user and associated roles

---
+++ Authentication/UserRolesDTO.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTSwagger.Authentication
+{
+    public class UserRolesDTO
+    {
+      [Required]
+      public string Id { get; set; }
+      [Required]
+      public string Email { get; set; }
+      [Required]
+      public string UserName { get; set; }
+      [Required]
+      public List<string> Roles { get; set; }
+    }
+}
