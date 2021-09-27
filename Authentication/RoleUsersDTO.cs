d1b2fc5a1b830ccbfece65056ae2c23bebfc060a
jgrissom <jgrissom91@gmail.com>
2021-09-26 22:56:33 -0500

    [2.07] Return specific role and associated users

---
+++ Authentication/RoleUsersDTO.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTSwagger.Authentication
+{
+    public class RoleUsersDTO
+    {
+      [Required]
+      public string Id { get; set; }
+      [Required]
+      public string Name { get; set; }
+      public List<string> Users { get; set; }
+    }
+}
--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -5,6 +5,7 @@ using JWTSwagger.Authentication;
 using Swashbuckle.AspNetCore.Annotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace JWTExample.Controllers
 {
@@ -14,9 +15,11 @@ namespace JWTExample.Controllers
   public class RolesController : Controller
   {
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public RolesController(RoleManager<IdentityRole> roleManager)
+    public RolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
     {
+      _userManager = userManager;
       _roleManager = roleManager;
     }
 
@@ -24,6 +27,28 @@ namespace JWTExample.Controllers
     [SwaggerOperation(summary: "Return all roles", null)]
     [SwaggerResponse(200, "Success", typeof(RoleDTO))]
     public IActionResult Get() => Ok(_roleManager.Roles.OrderBy(r => r.Name).Select(r => new RoleDTO { Id = r.Id, Name = r.Name }));
+
+    [HttpGet("{rolename}")]
+    [SwaggerOperation(summary: "Return specific role & associated users", null)]
+    [SwaggerResponse(200, "Success", typeof(RoleUsersDTO))]
+    public async Task<IActionResult> Get([FromRoute] string rolename)
+    {
+      // Check if role exists
+      var role = await _roleManager.FindByNameAsync(rolename);
+      if (role == null)
+        return NotFound(new { Message = "Role Not Found"});
+
+      // determine list of users assigned to role
+      List<string> UsersAssigned = new List<string>();
+      foreach(ApplicationUser user in _userManager.Users.ToList())
+      {
+        if (await _userManager.IsInRoleAsync(user, role.Name))
+        {
+          UsersAssigned.Add(user.UserName);
+        }
+      }
+      return Ok(new RoleUsersDTO { Id = role.Id, Name = role.Name , Users = UsersAssigned });
+    }
 
     [HttpPost]
     [Route("create")]
