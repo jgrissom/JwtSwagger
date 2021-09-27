ba44acf2879a3af86c7a749dbca3aa6073ac872f
jgrissom <jgrissom91@gmail.com>
2021-09-26 22:04:40 -0500

    [1.16] User login

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Linq;
 using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace JWTSwagger.Controllers
 {
@@ -16,10 +22,12 @@ namespace JWTSwagger.Controllers
   {
     // access UserManager w/ dependecy injection
     private readonly UserManager<ApplicationUser> _userManager;
-    public UsersController(UserManager<ApplicationUser> userManager)
+    private readonly IConfiguration _configuration;
+    public UsersController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
     {
       _userManager = userManager;
-     }
+      _configuration = configuration;
+    }
 
     [HttpGet]
     [SwaggerOperation(summary: "Return all users", null)]
@@ -56,6 +64,48 @@ namespace JWTSwagger.Controllers
         return StatusCode(StatusCodes.Status500InternalServerError);
 
       return NoContent();
+    }
+
+    [HttpPost]
+    [Route("login")]
+    [SwaggerOperation(summary: "User login", null)]
+    [SwaggerResponse(200, "Success", typeof(UserTokenDTO))]
+    public async Task<IActionResult> Login([FromBody] UserLogin model)
+    {
+      var user = await _userManager.FindByNameAsync(model.Username);
+      if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+      {
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        var authClaims = new List<Claim>
+        {
+          new Claim(JwtRegisteredClaimNames.Email, user.Email),
+          new Claim(JwtRegisteredClaimNames.Jti, user.Id),
+          new Claim("username", user.UserName),
+        };
+
+        foreach (var userRole in userRoles)
+        {
+          authClaims.Add(new Claim("roles", userRole));
+        }
+
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+        var token = new JwtSecurityToken(
+          issuer: _configuration["JWT:ValidIssuer"],
+          audience: _configuration["JWT:ValidAudience"],
+          expires: DateTime.Now.AddHours(3),
+          claims: authClaims,
+          signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return Ok(new UserTokenDTO
+        {
+          Token = new JwtSecurityTokenHandler().WriteToken(token),
+          Expiration = Convert.ToString(token.ValidTo)
+        });
+      }
+      return Unauthorized(new { Message = "Login falied" });
     }
 
     [HttpPost]
