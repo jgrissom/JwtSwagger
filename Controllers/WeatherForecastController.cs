aa00535008dbbd1f154b3abb5342e0851794f623
jgrissom <jgrissom91@gmail.com>
2021-09-26 22:07:47 -0500

    [1.17] Require authorization (WeatherForecast)

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JwtSwagger.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
